4e1630baa8e0f34863aa74d4efef6b47223adb4e
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2021-11-19 18:07:10 +0300

    Added click events to sercive images to toggle when clicked

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$()
+// Toggle for design
+$(".one").click(function () { 
+    $(".service1").slideToggle();    
+});
+// toggle for Development
+$(".two").click(function () { 
+    $(".service2").slideToggle();    
+})
+// toggle for pproduct management
+$(".three").click(function () { 
+    $(".service3").slideToggle();    
+})
